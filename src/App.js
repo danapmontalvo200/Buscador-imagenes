3a548d63b6fc6896b3cf294a25592dcb54b062c7
danapmontalvo200 <danapaolamontalvocuna@gmail.com>
2020-02-25 00:58:00 -0600

    consumi appi, falta perfeccionar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "bootswatch/dist/minty/bootstrap.min.css";
+import Buscador from './components/Buscador';
+import Resultado from './components/Resultado'
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+state = {
+    termino : '',
+    imagenes : [],
+    paguina : '',
+};
+
+pagAnterior = () => {
+  //console.log('Anterior...');
+   //leer el state de la pag actual
+let paguina = this.state.paguina;
+//leer si la paguina es 1 no retroceder
+if(paguina === 1) return null;
+//sumar una a la paguna actual
+paguina -= 1;
+//agregar el cambio al state
+this.setState({
+  paguina
+})
+
+};
+pagSiguiente = () => {
+ // console.log('Siguiente...')
+ //leer el state de la pag actual
+let paguina = this.state.paguina;
+ //sumar una a la paguna actual
+paguina += 1;
+ //agregar el cambio al state
+ this.setState({
+   paguina
+ })
+
+ console.log(paguina)
+};
+
+consultarApi = () => {
+
+  const termino = this.state.termino;
+  const url = `https://pixabay.com/api/?key=15380340-c0f2721d438f3d326df81a841&q=${termino}`;
+
+  //console.log(url);
+fetch(url)
+.then(respuesta => respuesta.json())
+.then (resultado => this.setState({ imagenes : resultado.hits}))
+
 }
+
+datosBusqueda = (termino) => {
+  this.setState({
+    termino : termino,
+    paguina : 1
+  }, () => {this.consultarApi();
+
+  });
+}
+
+render () {
+  return (
+    <div className="App container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de imagenes</p>
+          <Buscador 
+         datosBusqueda={this.datosBusqueda}
+          />
+        </div>     
+          <div className="row justify-content-center">
+            <Resultado 
+           imagenes={this.state.imagenes}
+           pagAnterior={this.pagAnterior}
+           pagSiguiente={this.pagSiguiente}
+           />
+         </div>
+    </div>
+    );
+  }
+}
+
 
 export default App;
